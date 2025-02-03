1e21502657cfb09c29714ba49afafb8db598f6e6
Pepegakac123 <kacpperr.adamczyk@gmail.com>
2025-02-03 09:57:38 +0100

    Change the enviroment to deno

--- src/test.test.ts
+++
@@ -1,8 +0,0 @@
-import { sum } from "./test";
-import { describe, expect, it } from "vitest";
-
-describe("sum function", () => {
-	it("should add two numbers", () => {
-		expect(sum(1, 2)).toBe(3);
-	});
-});
