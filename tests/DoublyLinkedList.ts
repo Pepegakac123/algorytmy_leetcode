c829ef9d49062262ed46c34dd37da84efc484881
Pepegakac123 <kacpperr.adamczyk@gmail.com>
2025-03-12 10:52:59 +0100

    Tests for doublylinkedlist

--- tests/DoublyLinkedList.ts
+++ tests/DoublyLinkedList.ts
@@ -1,7 +1,41 @@
-import LinkedList from "@code/DoublyLinkedList";
-import { test_list } from "./ListTest";
+import { describe, it } from "jsr:@std/testing/bdd";
+import { expect } from "@std/expect";
+import LinkedList from "../dataStructures/LinkedList.ts";
+describe("DoublyLinkedList", () => {
+	it("should perform basic operations correctly", () => {
+		// Setup a new list
+		const list = new LinkedList<number>();
 
-test("DoublyLinkedList", function () {
-    const list = new LinkedList<number>();
-    test_list(list);
+		// Test append operations
+		list.append(5);
+		list.append(7);
+		list.append(9);
+
+		// Test get operation
+		expect(list.get(2)).toEqual(9);
+
+		// Test removeAt operation
+		expect(list.removeAt(1)).toEqual(7);
+		expect(list.length).toEqual(2);
+
+		// Test more operations in sequence
+		list.append(11);
+		expect(list.removeAt(1)).toEqual(9);
+		expect(list.remove(9)).toEqual(undefined);
+		expect(list.removeAt(0)).toEqual(5);
+		expect(list.removeAt(0)).toEqual(11);
+		expect(list.length).toEqual(0);
+
+		// Test prepend operations
+		list.prepend(5);
+		list.prepend(7);
+		list.prepend(9);
+
+		// Test combined operations
+		expect(list.get(2)).toEqual(5);
+		expect(list.get(0)).toEqual(9);
+		expect(list.remove(9)).toEqual(9);
+		expect(list.length).toEqual(2);
+		expect(list.get(0)).toEqual(7);
+	});
 });
--- tests/ListTest.ts
+++ tests/ListTest.ts
@@ -1,26 +1,42 @@
-export function test_list(list: List<number>): void {
-    list.append(5);
-    list.append(7);
-    list.append(9);
+import { describe, it } from "jsr:@std/testing/bdd";
+import { expect } from "@std/expect";
+import LinkedList from "../dataStructures/LinkedList.ts";
 
-    expect(list.get(2)).toEqual(9);
-    expect(list.removeAt(1)).toEqual(7);
-    expect(list.length).toEqual(2);
+describe("DoublyLinkedList", () => {
+	it("should perform basic operations correctly", () => {
+		// Setup a new list
+		const list = new LinkedList<number>();
 
-    list.append(11);
-    expect(list.removeAt(1)).toEqual(9);
-    expect(list.remove(9)).toEqual(undefined);
-    expect(list.removeAt(0)).toEqual(5);
-    expect(list.removeAt(0)).toEqual(11);
-    expect(list.length).toEqual(0);
+		// Test append operations
+		list.append(5);
+		list.append(7);
+		list.append(9);
 
-    list.prepend(5);
-    list.prepend(7);
-    list.prepend(9);
+		// Test get operation
+		expect(list.get(2)).toEqual(9);
 
-    expect(list.get(2)).toEqual(5);
-    expect(list.get(0)).toEqual(9);
-    expect(list.remove(9)).toEqual(9);
-    expect(list.length).toEqual(2);
-    expect(list.get(0)).toEqual(7);
-}
+		// Test removeAt operation
+		expect(list.removeAt(1)).toEqual(7);
+		expect(list.length).toEqual(2);
+
+		// Test more operations in sequence
+		list.append(11);
+		expect(list.removeAt(1)).toEqual(9);
+		expect(list.remove(9)).toEqual(undefined);
+		expect(list.removeAt(0)).toEqual(5);
+		expect(list.removeAt(0)).toEqual(11);
+		expect(list.length).toEqual(0);
+
+		// Test prepend operations
+		list.prepend(5);
+		list.prepend(7);
+		list.prepend(9);
+
+		// Test combined operations
+		expect(list.get(2)).toEqual(5);
+		expect(list.get(0)).toEqual(9);
+		expect(list.remove(9)).toEqual(9);
+		expect(list.length).toEqual(2);
+		expect(list.get(0)).toEqual(7);
+	});
+});
