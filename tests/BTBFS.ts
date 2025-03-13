dc0fb1030d59763487d6db86c701a749e730f4db
Pepegakac123 <kacpperr.adamczyk@gmail.com>
2025-03-13 13:16:51 +0100

    Implement test for the tree tasks

--- tests/BTBFS.ts
+++ tests/BTBFS.ts
@@ -1,12 +1,12 @@
-import bfs from "@code/BTBFS";
-import { tree } from "./tree";
+import { bfs } from "../path/to/BTBFS.ts";
+import { tree } from "./tree.ts";
+import { describe, it } from "jsr:@std/testing/bdd";
+import { expect } from "jsr:@std/expect";
 
-test("bt bfs", function () {
-    expect(bfs(tree, 45)).toEqual(true);
-    expect(bfs(tree, 7)).toEqual(true);
-    expect(bfs(tree, 69)).toEqual(false);
+describe("Binary Tree Breadth-First Search", () => {
+	it("should correctly determine if values exist in the tree", () => {
+		expect(bfs(tree, 45)).toBe(true);
+		expect(bfs(tree, 7)).toBe(true);
+		expect(bfs(tree, 69)).toBe(false);
+	});
 });
-
-
-
-
--- tests/CompareBinaryTrees.ts
+++ tests/CompareBinaryTrees.ts
@@ -1,12 +1,12 @@
-import compare from "@code/CompareBinaryTrees";
-import { tree, tree2 } from "./tree";
+// CompareBinaryTrees.test.ts
+import { compare } from "../path/to/CompareBinaryTrees.ts";
+import { tree, tree2 } from "./tree.ts";
+import { describe, it } from "jsr:@std/testing/bdd";
+import { expect } from "jsr:@std/expect";
 
-test("Compare Binary Trees", function () {
-    expect(compare(tree, tree)).toEqual(true);
-    expect(compare(tree, tree2)).toEqual(false);
+describe("Compare Binary Trees", () => {
+	it("should correctly compare two binary trees", () => {
+		expect(compare(tree, tree)).toBe(true);
+		expect(compare(tree, tree2)).toBe(false);
+	});
 });
-
-
-
-
-
